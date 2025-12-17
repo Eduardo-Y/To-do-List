73868b6684ac15ddb642f7a470c806ab7a205f2c
Eduardo-Y <eduardodemelolive@outlook.com>
2025-12-16 21:17:05 -0300

    Adicionando nova funcionalidade e ajustando o código JS

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
+// Verificando se a tecla enter foi pressionada
+document.querySelector("#taskInput").addEventListener("keypress", teclaEnter);
+
+// Adicionando tarefa caso a tecla enter tenha sido pressionada
+function teclaEnter(event) {
+    if (event.key == "Enter") {
+        adicionarTarefa();
+    }
+}
+
 function adicionarTarefa() {
+    // Selecionando elementos: menu e input
     const menu = document.getElementById("menu");
     const input = document.querySelector("input");
-    let lista = null;
 
+    let lista;
+
+    // Verificando se a lista de tarefas existe
     if (menu.querySelector("ul")) {
+        // Caso exista: Seleciona ela
         lista = document.querySelector("ul");
     } else {
+        // Caso contrário: Cria e depois a seleciona
         lista = document.createElement("ul");
         menu.appendChild(lista);
     }
-    const novoItem = document.createElement("li");
 
-    const paragrafo = document.createElement("p");
-    paragrafo.textContent = input.value;
+    // Criando o novo item da lista (tarefa), botão de deletar tarefa, parágrafo (texto dentro da tarefa) e botão de tarefa feita (check)
+    const novoItem = document.createElement("li");
 
     const delButton = document.createElement("button");
     delButton.textContent = "X";
@@ -21,12 +35,18 @@ function adicionarTarefa() {
         lista.removeChild(novoItem);
     };
 
+    const paragrafo = document.createElement("p");
+    paragrafo.textContent = input.value;
+
     const check = document.createElement("input");
     check.className = "check";
     check.type = "checkbox";
 
+    // Conectando os elementos delButton, 'paragrafo' e check dentro do item
     novoItem.appendChild(delButton);
     novoItem.appendChild(paragrafo);
     novoItem.appendChild(check);
+
+    // Ligando a nova tarefa a lista
     lista.appendChild(novoItem);
 }
