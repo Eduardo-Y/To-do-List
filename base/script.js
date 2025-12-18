3bcb5b74b9e233ea6279ce1ac685ca0927c3b32a
Eduardo-Y <eduardodemelolive@outlook.com>
2025-12-18 19:43:39 -0300

    Adicionando funcionalidade de armazenamento das tarefas

--- base/script.js
+++ base/script.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    let tarefas = localStorage.getItem("tarefas");
+    if (tarefas) {
+        tarefas = tarefas.split(",");
+    }
+    const lista = document.querySelector("ul");
+
+    for (let i = 0; i < tarefas.length; i++) {
+        adicionarTarefa(tarefas[i]);
+    }
+});
+
 // Verificando se a tecla enter foi pressionada
 document.querySelector("#taskInput").addEventListener("keypress", teclaEnter);
 
@@ -8,36 +20,62 @@ function teclaEnter(event) {
     }
 }
 
-function adicionarTarefa() {
+function adicionarTarefa(tarefa = null) {
     // Selecionando elementos: menu e input
     const menu = document.getElementById("menu");
     const input = document.querySelector("input");
 
-    let lista;
+    let lista, tarefasStorage;
 
     // Verificando se a lista de tarefas existe
     if (menu.querySelector("ul")) {
         // Caso exista: Seleciona ela
         lista = document.querySelector("ul");
+        tarefasStorage = localStorage.getItem("tarefas").split(",");
     } else {
         // Caso contrário: Cria e depois a seleciona
         lista = document.createElement("ul");
         menu.appendChild(lista);
+        // Verificando se uma tarefa é nova ou apenas esta sendo carregada
+        if (!tarefa) {
+            localStorage.setItem("tarefas", "");
+        }
+        // Atribui um array com o nome as tarefas na ordem correta em uma variável
+        tarefasStorage = localStorage.getItem("tarefas").split("");
     }
 
-    // Criando o novo item da lista (tarefa), botão de deletar tarefa, parágrafo (texto dentro da tarefa) e botão de tarefa feita (check)
+    // Criando o novo item da lista (tarefa)
     const novoItem = document.createElement("li");
 
+    // parágrafo (texto dentro da tarefa)
+    const paragrafo = document.createElement("p");
+    // Verificando se uma tarefa é nova ou apenas esta sendo carregada
+    if (tarefa) {
+        paragrafo.textContent = tarefa;
+    } else {
+        paragrafo.textContent = input.value;
+
+        // Armazenando a tarefa
+        let arrayTarefas = tarefasStorage.concat(input.value);
+        localStorage.setItem("tarefas", `${arrayTarefas}`);
+    }
+
+    // Criando botão de deletar tarefa
     const delButton = document.createElement("button");
     delButton.textContent = "X";
     delButton.id = "delButton";
     delButton.onclick = () => {
+        // Remove o elemento "novoItem" da lista
         lista.removeChild(novoItem);
+
+        // Remove a tarefa do armazenamento local do navegador
+        tarefasStorage = localStorage.getItem("tarefas").split(",");
+        const index = tarefasStorage.indexOf(paragrafo.textContent);
+        tarefasStorage.splice(index, 1);
+        localStorage.setItem("tarefas", `${tarefasStorage}`);
     };
 
-    const paragrafo = document.createElement("p");
-    paragrafo.textContent = input.value;
-
+    // botão de tarefa feita (check)
     const check = document.createElement("input");
     check.className = "check";
     check.type = "checkbox";
